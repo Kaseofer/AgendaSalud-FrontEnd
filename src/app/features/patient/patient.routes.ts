ac35bac16a00face3372af18a13f3b0f3ad2b063
Fernando Garin <fgarin@JUSTICIA.AR>
2025-09-17 15:35:06 -0300

    first commit

--- src/app/features/patient/patient.routes.ts
+++ src/app/features/patient/patient.routes.ts
@@ -5,7 +5,7 @@ export const patientRoutes: Routes = [
     path: '', 
     loadComponent: () => import('../../layout/main-layout.component').then(c => c.MainLayoutComponent),
     children: [
-      { path: 'dashboard', loadComponent: () => import('./components/dashboard.component').then(c => c.PatientDashboardComponent) },
+      { path: 'dashboard', loadComponent: () => import('./components/dashboard/dashboard.component').then(c => c.PatientDashboardComponent) },
       { path: 'appointments', loadComponent: () => import('./components/appointments.component').then(c => c.PatientAppointmentsComponent) },
       { path: 'request-appointment', loadComponent: () => import('./components/request-appointment.component').then(c => c.RequestAppointmentComponent) },
       { path: 'history', loadComponent: () => import('./components/history.component').then(c => c.PatientHistoryComponent) },
